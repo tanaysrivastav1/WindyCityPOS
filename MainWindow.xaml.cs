8815587373bfda7efe6071568db20926d6b239cb
mertbayturk@gmail.com <mertbayturk@gmail.com>
2020-06-24 19:07:07 -0400

    calc

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -37,7 +37,7 @@ namespace WindyCityPOS
 
         private void Button_Click_2(object sender, RoutedEventArgs e)
         {
-
+           
         }
 
         private void Button_Click_3(object sender, RoutedEventArgs e)
